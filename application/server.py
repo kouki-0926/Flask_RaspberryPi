ce10b4c73602ed67bc75f5e24b937ad600d8c78e
Terakado Koki <53304697+terakou37910@users.noreply.github.com>
2020-10-31 17:12:16 +0900

    update server.py

--- application/server.py
+++ application/server.py
@@ -9,20 +9,19 @@ from flask_CPU.views import cpu
 from flask_math.views import Math
 from main.views import main
 
-app=Flask(__name__)
-app.config.from_object("config")
-
-update_weather()
-# update_CPU()
 sched = BackgroundScheduler(standalone=True, coalesce=True)
 sched.add_job(update_weather, 'interval', minutes=10)
-# sched.add_job(update_CPU, 'interval', seconds=1)
+sched.add_job(update_CPU, 'interval', minutes=10)
 sched.start()
+
+app=Flask(__name__)
+app.config.from_object("config")
 
 app.register_blueprint(main)
 app.register_blueprint(Math,url_prefix="/flask_math")
 app.register_blueprint(cpu,url_prefix="/flask_CPU")
 
 if __name__=="__main__":
+    update_weather()
     app.run()
     # app.run("0.0.0.0",port=5000)
