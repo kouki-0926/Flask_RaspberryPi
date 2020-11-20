9606be941620eff67e20b71df032a99d69633e39
Terakado Koki <53304697+terakou37910@users.noreply.github.com>
2020-11-20 19:13:31 +0900

    update flask_math

--- application/flask_math/calculation/taylor.py
+++ application/flask_math/calculation/taylor.py
@@ -1,6 +1,6 @@
 from sympy import *
 from flask import flash
-from flask_math.calculation.common.STR import STR
+from flask_math.calculation.common.STR import LATEX
 
 x = Symbol('x')
 
@@ -15,10 +15,9 @@ def taylor(formula,dimension,center):
             D=f.subs(x,center)/factorial(number)
             A=D*(x-center)**number+A
 
-        anser_1="f(x)="+STR(formula)
-        anser_2="f(x)≒"+STR(A)
+        anser_1="f(x)="+LATEX(formula)
+        anser_2="f(x)≒"+LATEX(A)
         Anser=[anser_1,anser_2]
-        print(Anser)
     except:
         Anser=["Error",""]
         flash("エラー：もう一度入力してください")
--- application/flask_math/views.py
+++ application/flask_math/views.py
@@ -311,13 +311,13 @@ def  newton_method_view():
 
 
 @Math.route("/prime_factorization",methods=["GET","POST"])
-def  prime_factorization_view():
+def prime_factorization_view():
     if request.method=="POST":
         number=request.form.get("number")
         anser=prime_factorization.prime_factorization(number)
-        return render_template("prime_factorization.html",number=number,anser=anser)
+        return render_template("prime_factorization.html",number=number,anser=anser,init_flag=0)
     else:
-        return render_template("prime_factorization.html")
+        return render_template("prime_factorization.html",init_flag=1)
 
 
 @Math.route("/Sieve_of_Eratosthenes",methods=["GET","POST"])
@@ -337,6 +337,6 @@ def  taylor_view():
         dimension=request.form.get("dimension")
         center=request.form.get("center")
         Anser=taylor.taylor(formula,dimension,center)
-        return render_template("taylor.html",formula=formula,dimension=dimension,center=center,Anser=Anser)
+        return render_template("taylor.html",formula=formula,dimension=dimension,center=center,Anser=Anser,init_flag=0)
     else:
-        return render_template("taylor.html",dimension=10,center=0)
+        return render_template("taylor.html",dimension=10,center=0,init_flag=1)
