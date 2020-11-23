1a9d9df093ac1f284421786274c4d73a8cab4b48
Terakado Koki <53304697+terakou37910@users.noreply.github.com>
2020-11-23 15:31:43 +0900

    update templates

--- application/flask_math/calculation/matrix_2.py
+++ application/flask_math/calculation/matrix_2.py
@@ -1,62 +1,33 @@
 from sympy import *
 from flask import flash
 from flask_math.calculation.common.MATRIX import MATRIX
+from flask_math.calculation.common.STR import LATEX
 
 def calculation(matrixA,matrixB,Ar,Ac,Br,Bc,type,k,l):
     try:
         Ar,Ac,Br,Bc,k,l=[int(Ar),int(Ac),int(Br),int(Bc),int(k),int(l)]
         A=MATRIX(matrixA,Ar,Ac)
         B=MATRIX(matrixB,Br,Bc)
-        type=type+" = "
-        output_type="MATRIX"
 
-        if type=="A = ":
-            anser=A
-            anser_r=Ar
-            anser_c=Ac
-
-        elif type=="B = ":
-            anser=B
-            anser_r=Br
-            anser_c=Bc
-
-        elif type=="kA+lB = ":
-            anser=k*A+l*B
-            type=str(k)+"A+"+str(l)+"B = "
-            anser_r=Ar
-            anser_c=Ac
-
-        elif type=="AB = ":
-            anser=A*B
-            anser_r=Ar
-            anser_c=Bc
-
-        elif type=="BA = ":
-            anser=B*A
-            anser_r=Br
-            anser_c=Ac
-
-        elif type=="A・B(内積) = ":
-            anser=B.dot(A)
-            anser_r=""
-            anser_c=""
-            output_type="NUMBER"
-
-        elif type=="A×B(外積) = ":
-            anser=A.cross(B)
-            anser_r=Ar
-            anser_c=Ac
-
-        elif type=="B×A(外積) = ":
-            anser=B.cross(A)
-            anser_r=Ar
-            anser_c=Ac
+        if type=="A":
+            anser=LATEX(A)
+        elif type=="B":
+            anser=LATEX(B)
+        elif type=="kA+lB":
+            anser=LATEX(k*A+l*B)
+            type=str(k)+"A+"+str(l)+"B"
+        elif type=="AB":
+            anser=LATEX(A*B)
+        elif type=="BA":
+            anser=LATEX(B*A)
+        elif type=="A・B(内積)":
+            anser=LATEX(B.dot(A))
+        elif type=="A×B(外積)":
+            anser=LATEX(A.cross(B))
+        elif type=="B×A(外積)":
+            anser=LATEX(B.cross(A))
+        anser=str(type)+"="+anser    
     except:
         anser="Error"
-        type=""
-        anser_r=""
-        anser_c=""
-        output_type="NUMBER"
         flash("エラー：もう一度入力してください")
-    Anser=[anser,type,anser_r,anser_c,output_type]
-    return Anser
+    return anser
--- application/flask_math/views.py
+++ application/flask_math/views.py
@@ -287,20 +287,10 @@ def  matrix_2_view():
         k=request.form.get("k")
         l=request.form.get("l")
 
-        Anser=matrix_2.calculation(matrixA,matrixB,Ar,Ac,Br,Bc,type,k,l)
-
-        anser=[]
-        if Anser[4]=="NUMBER":
-            anser=[Anser[0]]
-        elif Anser[4]=="MATRIX":
-            for i in range(Anser[2]):
-                A=str(Anser[0].row(i))
-                A=A.replace("Matrix","").replace("**","^").replace("*","").replace("([[","[").replace("]])","]")
-                anser.append(A)
-        return render_template("matrix_2.html",matrixA=matrixA,matrixB=matrixB,Ar=Ar,Ac=Ac,Br=Br,Bc=Bc,type=type,k=k,l=l,
-        anser_0=anser,anser_1=Anser[1])
+        anser=matrix_2.calculation(matrixA,matrixB,Ar,Ac,Br,Bc,type,k,l)
+        return render_template("matrix_2.html",matrixA=matrixA,matrixB=matrixB,Ar=Ar,Ac=Ac,Br=Br,Bc=Bc,type=type,k=k,l=l,anser=anser,init_flag=0)
     else:
-        return render_template("matrix_2.html",Ar=2,Ac=2,Br=2,Bc=2,type="A",k=2,l=2)
+        return render_template("matrix_2.html",Ar=2,Ac=2,Br=2,Bc=2,type="A",k=2,l=2,init_flag=1)
 
 
 @Math.route("/max_min",methods=["GET","POST"])
