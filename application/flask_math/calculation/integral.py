27bbbbadbb9e86c934f3ad41a8b827f170677f4f
Terakado Koki <53304697+terakou37910@users.noreply.github.com>
2020-12-29 00:07:49 +0900

    update template

--- application/flask_math/calculation/integral.py
+++ application/flask_math/calculation/integral.py
@@ -1,30 +1,31 @@
-from sympy import symbols,integrate
+from sympy import symbols, integrate
 from flask import flash
 from flask_math.calculation.common.STR import LATEX
 
-x,y=symbols('x y')
+x, y = symbols('x y')
 
-def integral(formula,Up,Low,type):
+
+def integral(formula, Up, Low, type):
     try:
-        if(type=="multiple_integral_1" or type=="multiple_integral_2"):
-            A=integrate(formula,(x,Low[0],Up[0]),(y,Low[1],Up[1]))
-            anser="\int_{"+LATEX(Low[1])+"}^{"+LATEX(Up[1])+"} \int_{"+LATEX(Low[0])+"}^{"+LATEX(Up[0])+"}"+LATEX(formula)+"dxdy="
-            if(type=="multiple_integral_1"):
-                anser+=LATEX(A)
-            elif(type=="multiple_integral_2"):
-                anser+=LATEX(A.evalf())
+        if(type == "multiple_integral_1" or type == "multiple_integral_2"):
+            A = integrate(formula, (x, Low[0], Up[0]), (y, Low[1], Up[1]))
+            anser = "\int_{"+LATEX(Low[1])+"}^{"+LATEX(Up[1])+"} \int_{"+LATEX(Low[0])+"}^{"+LATEX(Up[0])+"}"+LATEX(formula)+"dxdy="
+            if(type == "multiple_integral_1"):
+                anser += LATEX(A)
+            elif(type == "multiple_integral_2"):
+                anser += LATEX(A.evalf())
         else:
-            g=integrate(formula,x)
-            if(type=="indefinite_integral"):
-                anser="\int "+LATEX(formula)+"dx="+LATEX(g)+"+C"
+            g = integrate(formula, x)
+            if(type == "indefinite_integral"):
+                anser = "\int "+LATEX(formula)+"dx="+LATEX(g)+"+C"
             else:
-                A=g.subs(x,Up[0])-g.subs(x,Low[0])
-                anser="\int_{"+LATEX(Low[0])+"}^{"+LATEX(Up[0])+"}"+LATEX(formula)+"dx="
-                if(type=="definite_integral_1"):
-                    anser+=LATEX(A)
-                elif(type=="definite_integral_2"):
-                    anser+=LATEX(A.evalf())
+                A = g.subs(x, Up[0])-g.subs(x, Low[0])
+                anser = "\int_{"+LATEX(Low[0])+"}^{"+LATEX(Up[0])+"}"+LATEX(formula)+"dx="
+                if(type == "definite_integral_1"):
+                    anser += LATEX(A)
+                elif(type == "definite_integral_2"):
+                    anser += LATEX(A.evalf())
     except:
-        anser="Error"
+        anser = "Error"
         flash("エラー:もう一度入力してください")
     return anser
