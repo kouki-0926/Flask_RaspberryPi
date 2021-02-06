263c0152e98b3e396802cb5961933e72444d7471
Terakado Koki <53304697+terakou37910@users.noreply.github.com>
2021-02-06 11:26:56 +0900

    update jyanken

--- application/flask_game/Game/jyanken.py
+++ application/flask_game/Game/jyanken.py
@@ -1,4 +1,21 @@
-def jyanken(n):
-    te[3] = ["グー", "チョキ", "パー"]
+from random import uniform
+from flask import flash
 
-    return "a"
+
+def jyanken(n):
+    try:
+        te = ["グー", "チョキ", "パー"]
+        nc = int(uniform(0, 100)) % 3+1
+
+        Anser = ["君は["+str(te[n-1])+"]か。"]
+
+        if (nc == n):
+            Anser.append("オラも["+te[nc - 1]+"]だ。あいこだな。")
+        elif ((nc - n == -2) or (nc - n == 1)):
+            Anser.append("オラは["+te[nc - 1]+"]だ。君の勝ちだな。")
+        else:
+            Anser.append("オラは["+te[nc - 1]+"]だ。オラの勝ちだな。")
+    except:
+        Anser = ["Error"]
+        flash("Error")
+    return Anser
--- application/flask_game/views.py
+++ application/flask_game/views.py
@@ -1,4 +1,4 @@
-from flask import redirect, url_for, render_template, flash, Blueprint, request
+from flask import redirect, render_template, flash, Blueprint, request
 from flask_game.Game import *
 
 game = Blueprint("game", __name__, template_folder='templates_game', static_folder="static_game")
@@ -12,8 +12,8 @@ def index_view():
 @game.route("/jyanken", methods=["GET", "POST"])
 def jyanken_view():
     if request.method == "POST":
-        n = request.form.get("n")
-        anser = Game.jyanken(n)
-        return render_template("jyanken.html", n=n, anser=anser)
+        n = int(request.form.get("n"))
+        Anser = jyanken.jyanken(n)
+        return render_template("jyanken.html", n=n, Anser=Anser)
     else:
-        return render_template("jyanken.html")
+        return render_template("jyanken.html", n=1)
