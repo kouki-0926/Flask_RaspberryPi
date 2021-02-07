07cc1186402cb00f458dc847853415f531a5ac42
Terakado Koki <53304697+terakou37910@users.noreply.github.com>
2021-02-07 23:04:13 +0900

    update bike

--- application/flask_game/static_game/js/bike.js
+++ application/flask_game/static_game/js/bike.js
@@ -7,12 +7,13 @@ var k = { ArrowUp: 0, ArrowDown: 0, ArrowLeft: 0, ArrowRight: 0 };
 
 // canvas 生成
 var c = document.createElement("canvas");
-var ctx = c.getContext("2d");
-c.width = 1300;
+c.width = 1480;
 c.height = 400;
+var ctx = c.getContext("2d");
 var parentDiv = document.getElementById("parent-div");
 var title = document.getElementById("title");
 parentDiv.insertBefore(c, title.nextSibling);
+c.classList.add('display-none');
 
 
 // 画面生成のパラメータ生成
@@ -45,7 +46,7 @@ var player = new function () {
         var p1 = c.height - noise(t + this.x) * 0.25;
         var p2 = c.height - noise(t + 5 + this.x) * 0.25;
 
-
+        // 縦移動のスピードの更新
         if (p1 - size > this.y) {
             this.ySpeed += 0.1;
         } else {
@@ -53,31 +54,28 @@ var player = new function () {
             this.y = p1 - size;
             grounded = 1;
         }
-
-        // gameover判定
-        if (!playing || grounded && Math.abs(this.rot) > Math.PI * 0.5) {
-            playing = false;
-            this.rSpeed = 15;
-            k.ArrowUp = 1;
-            this.x -= speed * 2;
-        }
-
-
-        var angle = Math.atan2((p2 - 10) - this.y, 5);
-
         this.y += this.ySpeed;
 
-
+        // 回転回転のスピードの更新
+        var angle = Math.atan2((p2 - 10) - this.y, 5);
         if (grounded && playing) {
             this.rot -= (this.rot - angle) * 0.5;
             this.rSpeed = this.rSpeed - (angle - this.rot);
         }
+        this.rSpeed += (k.ArrowRight - k.ArrowLeft) * 0.5;
+        this.rot += this.rSpeed * 0.01;
 
-        this.rSpeed += (k.ArrowLeft - k.ArrowRight) * 0.5;
-        this.rot += this.rSpeed * 0.03;
-
+        // 回転しすぎを修正
         if (this.rot > Math.PI) this.rot = -Math.PI;
         if (this.rot < -Math.PI) this.rot = Math.PI;
+
+        // gameover判定
+        if (!playing || grounded && Math.abs(this.rot) > Math.PI * 0.8) {
+            playing = false;
+            this.rSpeed = 15;
+            k.ArrowUp = 1;
+            this.x -= speed * 15;
+        }
 
 
         ctx.save();
@@ -93,16 +91,15 @@ function loop() {
         speed -= (speed - (k.ArrowUp - k.ArrowDown)) * 0.1;
         t += 10 * speed;
     } else {
-        t += 50 * speed;
+        t += 30;
     }
 
     ctx.fillStyle = "#19f";
     ctx.fillRect(0, 0, c.width, c.height);
     ctx.fillStyle = "black";
-
     ctx.beginPath();
-    ctx.moveTo(0, c.height);
 
+    ctx.moveTo(0, c.height);
     for (var i = 0; i < c.width; i++) {
         ctx.lineTo(i, c.height - noise(t + i) * 0.25);
     }
@@ -110,7 +107,6 @@ function loop() {
     ctx.fill();
 
     player.draw();
-
     requestAnimationFrame(loop);
 }
 
