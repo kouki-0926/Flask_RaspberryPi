5f1fa0a152888745f5a1c249afb8a26bd4f610a3
Kouki-0926 <terakou37910@icloud.com>
2021-03-25 15:53:42 +0900

    update janken

--- application/flask_game/views.py
+++ application/flask_game/views.py
@@ -1,4 +1,4 @@
-from flask import request, redirect, url_for, render_template, flash, Blueprint
+from flask import request, redirect, url_for, render_template, flash, Blueprint, session
 from flask_game.Game import *
 
 game = Blueprint("game", __name__,
@@ -16,26 +16,30 @@ def janken_view():
     type = request.args.get("type")
     if(type == "ml"):
         if(n == '1' or n == '2' or n == '3'):
-            Anser = janken_ml.janken_ml(int(n))
+            Anser = janken_ml.janken_ml(int(n), session.get('result_ml', 'aaa'))
+            session['result_ml'] = Anser[2]
+            print(session.get('result_ml', 'aaa'))
             return render_template("janken.html", type=type, n=Anser[0][0], nc=Anser[0][1], Anser=Anser[1])
         elif(n == "init"):
-            janken_ml.janken_ml_reset()
+            session['result_ml'] = [0, 0, 0]
             return render_template("janken.html", type=type)
         elif(n == "reset"):
-            janken_ml.janken_ml_reset()
+            session['result_ml'] = [0, 0, 0]
             flash("正常に勝敗結果が初期化されました")
             return render_template("janken.html", type=type)
         else:
             return redirect(url_for("game.janken_view", type="ml", n="init"))
     elif(type == "rm"):
         if(n == '1' or n == '2' or n == '3'):
-            Anser = janken.janken(int(n))
+            Anser = janken.janken(int(n), session.get('result_rm', 'aaa'))
+            session['result_rm'] = Anser[2]
+            print(session.get('result_ml', 'aaa'))
             return render_template("janken.html", type=type, n=Anser[0][0], nc=Anser[0][1], Anser=Anser[1])
         elif(n == "init"):
-            janken.janken_reset()
+            session['result_rm'] = [0, 0, 0]
             return render_template("janken.html", type=type)
         elif(n == "reset"):
-            janken.janken_reset()
+            session['result_rm'] = [0, 0, 0]
             flash("正常に勝敗結果が初期化されました")
             return render_template("janken.html", type=type)
         else:
